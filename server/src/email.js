9dd83c02dfeb6a16985e1c2c1832dd4595585082
Atanas <bobevatanas@gmail.com>
2021-12-30 21:46:06 +0200

    main

--- server/src/email.js
+++ server/src/email.js
@@ -1,59 +1,30 @@
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "unpopular.noreply@gmail.com",
+    pass: "RobertZein2704$",
+  },
+});
 const sendMail = (subject, text, to, attachments) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "bobevatanas2704@gmail.com",
-        pass: "Atanasko",
-      },
-    });
-    transporter.sendMail(
-      {
-        from: "Unpopular",
-        to: to,
-        subject: subject,
-        text: text,
-        attachments: attachments,
-      },
-      function (error, info) {
-        if (error) {
-          console.log(error);
-          return false;
-        } else {
-          console.log("Email sent: " + info.response);
-          return true;
-        }
+  transporter.sendMail(
+    {
+      from: "Unpopular.bg",
+      to: to,
+      subject: subject,
+      text: text,
+      attachments: attachments,
+    },
+    function (error, info) {
+      if (error) {
+        console.log(error);
+        return false;
+      } else {
+        console.log("Email sent: " + info.response);
+        return true;
       }
-    );
-  } catch (err) {
-    console.log(err);
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "unpopular.noreply@gmail.com",
-        pass: "RobertZein2704$",
-      },
-    });
-    transporter.sendMail(
-      {
-        from: "Unpopular",
-        to: to,
-        subject: subject,
-        text: text,
-        attachments: attachments,
-      },
-      function (error, info) {
-        if (error) {
-          console.log(error);
-          return false;
-        } else {
-          console.log("Email sent: " + info.response);
-          return true;
-        }
-      }
-    );
-  }
+    }
+  );
 };
 module.exports = sendMail;
